0d034df3de55476d7d49b74013fa1f0beb493181
AtleSkaanes <atle.skaanes@gmail.com>
2023-10-18 21:19:39 +0200

    I think this is gonna be better

--- lua/telescope/_extensions/telescopeplugins.lua
+++ lua/telescope/_extensions/telescopeplugins.lua
@@ -15,12 +15,22 @@ return require("telescope").register_extension {
     exports = {
         ListPlugins = function(opts)
             opts = opts or {}
-            print("should work")
             pickers.new(opts, {
                 prompt_title = "plugins",
                 sorter = sorters.get_generic_fuzzy_sorter(),
                 finder = finders.new_table {
-                    results = { "red", "green", "blue" }
+                    results = {
+                        { "red", "#ff0000" },
+                        { "green", "#00ff00" },
+                        { "blue", "#0000ff" },
+                    },
+                    entry_maker = function(entry)
+                        return {
+                            value = entry,
+                            display = entry[1],
+                            ordinal = entry[1],
+                        }
+                    end
                 },
                 attach_mappings = function(prompt_bufnr, map)
                     actions.select_default:replace(function()
