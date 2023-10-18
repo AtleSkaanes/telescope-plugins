1f157ce09cfa6a9c232e9e84c94b3eef4fe03585
AtleSkaanes <atle.skaanes@gmail.com>
2023-10-18 21:05:55 +0200

    basic telescope setup

---
+++ lua/telescope/_extensions/telescopeplugins.lua
@@ -0,0 +1,33 @@
+local pickers = require "telescope.pickers"
+local finders = require "telescope.finders"
+local conf = require("telescope.config").values
+local sorters = require("telescope.sorters")
+local actions = require "telescope.actions"
+local action_state = require "telescope.actions.state"
+
+
+local get_plugins = function ()
+
+end
+
+
+return require("telescope").register_extension {
+    exports = {
+        ListPlugins = function(opts)
+            opts = opts or {}
+
+            pickers.new(opts, {
+                prompt_title = "plugins",
+                sorter = sorters.get_generic_fuzzy_sorter(),
+                attach_mappings = function(prompt_bufnr, map)
+                    actions.select_default:replace(function()
+                        actions.close(prompt_bufnr)
+                        local selection = action_state.get_selected_entry()
+                        print(vim.inspect(selection))
+                    end)
+                    return true
+                end,
+            })
+        end,
+    }
+}
